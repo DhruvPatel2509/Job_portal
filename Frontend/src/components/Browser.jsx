d9d88635fac0f812d4b62ef0c64d24f325f3788e
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2024-08-14 23:01:03 +0530

    Browse page done

--- Frontend/src/components/Browser.jsx
+++ Frontend/src/components/Browser.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Jobcard from "../components/Jobcard";
 
 function Browser() {
-  return <>
-    
-  </>;
+  const randomJobs = [1, 1, 1];
+  return (
+    <>
+      <div className="max-w-7xl mx-auto my-10">
+        <h1 className="font-bold text-xl my-10">
+          Search Results ({randomJobs.length})
+        </h1>
+        <div className="grid grid-cols-3 gap-4 ">
+          {randomJobs.map((item, index) => (
+            <div>
+              <Jobcard />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Browser;
