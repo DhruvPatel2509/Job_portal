b1427d6b9cd15ee50e240a403454ce00c9308724
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2024-08-19 22:00:12 +0530

    Redux-persisor

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,10 +4,16 @@ import "./index.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+const persistor = persistStore(store);
 createRoot(document.getElementById("root")).render(
   <>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
     <Toaster />
   </>
