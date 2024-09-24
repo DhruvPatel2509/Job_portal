42a652bd6482e9024ac9a8253a78ab2fe13c54c9
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2024-09-24 23:22:16 +0530

    jobtable

--- Frontend/src/components/admin/Applicants.jsx
+++ Frontend/src/components/admin/Applicants.jsx
@@ -1,13 +1,42 @@
+import { useEffect } from "react";
 import Applicantstable from "./Applicantstable";
+import axios from "axios";
+import { APPLICATION_API_END_POINT } from "../../utils/constant";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAllApplicants } from "../../redux/applications";
 
 function Applicants() {
+  const params = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchApplicants = async () => {
+      try {
+        const res = await axios.get(
+          `${APPLICATION_API_END_POINT}/getApplicant/${params.id}`,
+          { withCredentials: true }
+        );
+
+        if (res.data.success) {
+          dispatch(setAllApplicants(res.data.data.application));
+        }
+      } catch (error) {
+        console.error("Error fetching applicants:", error);
+      }
+    };
+    fetchApplicants();
+  }, [params.id, dispatch]); // Include params.id as a dependency
+
+  const { applicants } = useSelector((store) => store.application);
+
   return (
-    <>
-      <div className="max-w-7xl mx-auto">
-        <h1 className="font-bold text-xl my-5">Applicants (3) </h1>
-        <Applicantstable />
-      </div>
-    </>
+    <div className="max-w-7xl mx-auto">
+      <h1 className="font-bold text-xl my-5">
+        Applicants ({applicants.length})
+      </h1>
+      <Applicantstable />
+    </div>
   );
 }
 
--- Frontend/src/components/admin/Applicantstable.jsx
+++ Frontend/src/components/admin/Applicantstable.jsx
@@ -7,7 +7,12 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useSelector } from "react-redux";
+
 function Applicantstable() {
+  const { applicants } = useSelector((store) => store.application);
+  console.log(applicants);
+
   return (
     <>
       <Table>
@@ -19,8 +24,31 @@ function Applicantstable() {
             <TableHead>Phone Number</TableHead>
             <TableHead>Resume</TableHead>
             <TableHead>Date</TableHead>
+            <TableHead className="text-right">Action</TableHead>
           </TableRow>
         </TableHeader>
+        <TableBody>
+          {applicants?.map((a) => (
+            <tr key={a._id}>
+              <TableCell>{a.applicant.fullname} </TableCell>
+              <TableCell>{a.applicant.email}</TableCell>
+              <TableCell>{a.applicant.phoneNumber}</TableCell>
+              <TableCell>
+                <a
+                  className="text-blue-600 cursor-pointer"
+                  href={a.applicant.profile.resume}
+                >
+                  {a.applicant.profile.resumeOrignalName}
+                </a>
+              </TableCell>
+              <TableCell>{a.applicant.createdAt.split("T")[0]}</TableCell>
+              <TableCell className="text-right">
+                <p>Accepted</p>
+                <p>Rejected</p>
+              </TableCell>
+            </tr>
+          ))}
+        </TableBody>
       </Table>
     </>
   );
