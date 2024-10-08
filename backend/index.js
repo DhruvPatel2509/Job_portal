7c4db59e2159636cd6fbef7be5e69df98368356c
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2024-10-08 18:28:09 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "*", 
+  origin: "https://job-portal-ofw5.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true, // Allows cookies to be sent
