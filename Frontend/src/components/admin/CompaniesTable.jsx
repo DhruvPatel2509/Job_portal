e27ded67b31c7a30852afefa1aea8cae49dd2d77
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2024-10-23 22:48:52 +0530

    Responsive

--- Frontend/src/components/admin/CompaniesTable.jsx
+++ Frontend/src/components/admin/CompaniesTable.jsx
@@ -1,4 +1,16 @@
-/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+import { Edit2Icon, MoreHorizontalIcon } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Popover,
@@ -6,41 +18,20 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Edit2Icon, MoreHorizontalIcon } from "lucide-react";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-
 function CompaniesTable() {
   const { allCompanies, searchCompanyByText } = useSelector(
     (state) => state.company
   );
-
-  const [filterCompany, setFilterCompany] = useState(allCompanies);
-
+  const [filteredCompanies, setFilteredCompanies] = useState(allCompanies);
   const navigate = useNavigate();
+
   useEffect(() => {
-    const filteredCompany =
-      allCompanies?.length > 0 &&
-      allCompanies?.filter((company) => {
-        if (!searchCompanyByText) {
-          return true;
-        }
-        return company?.name
-          ?.toLowerCase()
-          .includes(searchCompanyByText.toLowerCase());
-      });
-    setFilterCompany(filteredCompany);
+    const filteredCompany = allCompanies?.filter(
+      (company) =>
+        !searchCompanyByText ||
+        company.name.toLowerCase().includes(searchCompanyByText.toLowerCase())
+    );
+    setFilteredCompanies(filteredCompany);
   }, [allCompanies, searchCompanyByText]);
 
   const editHandler = (id) => {
@@ -48,52 +39,49 @@ function CompaniesTable() {
   };
 
   return (
-    <>
-      <Table>
-        <TableCaption>A List Of Your Recent Registered Companies</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Logo</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead className="text-right">Action</TableHead>
+    <Table>
+      <TableCaption>A List Of Your Recent Registered Companies</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Logo</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Date</TableHead>
+          <TableHead className="text-right">Action</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {filteredCompanies?.map((company) => (
+          <TableRow key={company._id}>
+            <TableCell>
+              <Avatar className="w-[80px] h-[45px]">
+                <AvatarImage src={company.logo} alt={`${company.name} Logo`} />
+                <AvatarFallback>{company.name.charAt(0)}</AvatarFallback>
+              </Avatar>
+            </TableCell>
+            <TableCell>{company.name}</TableCell>
+            <TableCell>
+              {new Date(company.createdAt).toLocaleDateString()}
+            </TableCell>
+            <TableCell className="text-right cursor-pointer">
+              <Popover>
+                <PopoverTrigger>
+                  <MoreHorizontalIcon />
+                </PopoverTrigger>
+                <PopoverContent
+                  className="w-32"
+                  onClick={() => editHandler(company._id)}
+                >
+                  <div className="flex items-center gap-2 cursor-pointer w-fit">
+                    <Edit2Icon className="w-4" />
+                    <span>Edit</span>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {filterCompany &&
-            filterCompany.map((c) => (
-              <TableRow key={c._id}>
-                <TableCell>
-                  <Avatar className="w-[80px] h-[45px]">
-                    <AvatarImage src={c.logo} alt={`${c.name} Logo`} />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </TableCell>
-                <TableCell>{c.name}</TableCell>
-                <TableCell>
-                  {new Date(c.createdAt).toLocaleDateString()}
-                </TableCell>
-                <TableCell className="text-right cursor-pointer">
-                  <Popover>
-                    <PopoverTrigger>
-                      <MoreHorizontalIcon />
-                    </PopoverTrigger>
-                    <PopoverContent
-                      className="w-32"
-                      onClick={() => editHandler(c._id)}
-                    >
-                      <div className="flex items-center gap-2 cursor-pointer w-fit">
-                        <Edit2Icon className="w-4" />
-                        <span>Edit</span>
-                      </div>
-                    </PopoverContent>
-                  </Popover>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
 
--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  // origin: "https://job-portal-ofw5.vercel.app",
+  // origin: "http://localhost:5173",
+  origin: "https://job-portal-ofw5.vercel.app",
 
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
