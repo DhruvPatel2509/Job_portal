20caad796df211b86190eea407ddbbc530e068b5
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2025-02-24 18:53:49 +0530

    Refactor Herosection component; wrap input and button in a form for better structure and accessibility

--- Frontend/src/components/Herosection.jsx
+++ Frontend/src/components/Herosection.jsx
@@ -35,17 +35,21 @@ export const Herosection = () => {
       </p>
 
       <div className="flex items-center w-full gap-2 px-4 py-2 mx-auto mt-8 bg-white border border-gray-300 rounded-full shadow-xl focus-within:ring-2 focus-within:ring-[#6A38C2] sm:w-[70%] md:w-[60%] lg:w-[40%]">
-        <input
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Enter job title, skills, or company"
-          className="w-full p-2 text-sm text-gray-700 placeholder-gray-500 bg-transparent border-none outline-none sm:text-base"
-        />
-        <Button
-          className="p-3 text-white transition-transform duration-200 ease-in-out transform rounded-full bg-[#6A38C2] hover:bg-[#5a2ca2] hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6A38C2]"
-          onClick={searchJobHandler}
-        >
-          <Search className="w-5 h-5" />
-        </Button>
+        <form className="w-full"> 
+          <div className="flex items-center justify-between  gap-2">
+            <input
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Enter job title, skills, or company"
+              className="w-full p-2 text-sm text-gray-700 placeholder-gray-500 bg-transparent border-none outline-none sm:text-base"
+            />
+            <Button
+              className="p-3 text-white transition-transform duration-200 ease-in-out transform rounded-full bg-[#6A38C2] hover:bg-[#5a2ca2] hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6A38C2]"
+              onClick={searchJobHandler}
+            >
+              <Search className="w-5 h-5" />
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
