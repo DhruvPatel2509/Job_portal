79013d842d3656c41cde054667da08bac7164bc8
DhruvPatel2509 <www.dhruvpatelpatel@gmail.com>
2025-03-21 09:02:05 +0530

    Update CORS configuration to use environment variable for origin

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  // origin: "http://localhost:5173",
-  origin: "https://job-portal-ofw5.vercel.app",
+  origin: process.env.ORIGIN,
 
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
